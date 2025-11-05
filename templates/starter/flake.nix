22568905ee704441a5217c329890e1e25141ca0f
Winst <67540662+verymucho@users.noreply.github.com>
2025-11-05 05:48:10 -0700

    flake.lock: Update (#2)
    Flake lock file updates:
    
    • Updated input 'homebrew-cask':
        'github:homebrew/homebrew-cask/f38aaa23ba1dcc6709aaae9a12224abd75a15170?narHash=sha256-mSTsyRPXy%2Bf7dUwnPbKQgZ0xr8Ktfk%2BP/yIPOb9ISpo%3D' (2025-11-05)
      → 'github:homebrew/homebrew-cask/2b5c58b7f3caa242fa435d0eb61352f5c5c8d5e7?narHash=sha256-sNhRh5/fdXcOoTnkVd5cuxNZT9FMxJzCJyWel32PMUM%3D' (2025-11-05)
    • Updated input 'homebrew-core':
        'github:homebrew/homebrew-core/2277be3d6538218e5cfa66d4fc33a87f2f859032?narHash=sha256-zdm1bdveJAlyeL4q4nDkAGkhs1qvMPHhXnDJi2HpaRw%3D' (2025-11-05)
      → 'github:homebrew/homebrew-core/c373265ec44bc655703945336459e05f4ace838d?narHash=sha256-ALZT0TKiqDzjkKUoOdW9mjbRNltJtCsgOB1mFZflRRY%3D' (2025-11-05)
    • Updated input 'homebrew-hashicorp':
        'github:hashicorp/homebrew-tap/97d4e7f73692718f667e98332f6f7c5d958c04cc?narHash=sha256-q7prbQQE/Io5TMnLDQ08qVXBWgGhiZmeVuB2s3KfcOE%3D' (2025-11-04)
      → 'github:hashicorp/homebrew-tap/f8399314513b270100995178b56ca7935b7be2a2?narHash=sha256-ewPov5o/twzDZ/16RPf%2BuaEm2US81DU3cZBn5K57NbQ%3D' (2025-11-05)
    • Updated input 'mac-app-util':
        'github:hraban/mac-app-util/8414fa1e2cb775b17793104a9095aabeeada63ef?narHash=sha256-ziR5eQGqRWhW8tf8r0TIplaqNt%2BHXu1G1X41LUr4IYo%3D' (2025-08-24)
      → 'github:slickag/mac-app-util/70b02a67feff5ca0c125552b52f614f854389bb4?narHash=sha256-pm26CJo8D5nWqFfnn0uh7ZqHoMNZ88vD3ciEJR8V8LA%3D' (2025-11-05)
    • Updated input 'mac-app-util/cl-nix-lite':
        'github:hraban/cl-nix-lite/31cfe6275c341eb3120a99f4b1c8516c49a29d87?narHash=sha256-Grqqg%2BxuicANB85j0gNEXxi9SBKY7bzGeTuyi95eGcY%3D' (2024-10-06)
      → 'github:slickag/cl-nix-lite/368c429bf6afbe8e9d35c8582d84231c7887a569?narHash=sha256-kl%2BonGpDktHaui0i/duaz5xT8b7CMnBFX45rYxieX78%3D' (2025-11-05)
    • Updated input 'mac-app-util/nixpkgs':
        'github:NixOS/nixpkgs/af51545ec9a44eadf3fe3547610a5cdd882bc34e?narHash=sha256-PYkz6U0bSEaEB1al7O1XsqVNeSNS%2Bs3NVclJw7YC43w%3D' (2024-11-26)
      → follows 'nixpkgs'
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/7241bcbb4f099a66aafca120d37c65e8dda32717?narHash=sha256-Yg7Ag7ov5%2B36jEFC1DaZh/12SEXo6OO3/8rqADRxiqs%3D' (2025-11-03)
      → 'github:NixOS/nixpkgs/12c1f0253aa9a54fdf8ec8aecaafada64a111e24?narHash=sha256-OD5HsZ%2BsN7VvNucbrjiCz7CHF5zf9gP51YVJvPwYIH8%3D' (2025-11-04)
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- templates/starter/flake.nix
+++ templates/starter/flake.nix
@@ -15,8 +15,8 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     mac-app-util = {
-      # url = "github:hraban/mac-app-util";
-      url = "github:slickag/mac-app-util";
+      url = "github:hraban/mac-app-util";
+      inputs.cl-nix-lite.url = "github:verymucho/cl-nix-lite/main";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     homebrew-core = {
